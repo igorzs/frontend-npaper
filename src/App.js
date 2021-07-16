edf883e47706c28c0c80e8e1b3edc4f2de485748
cristianohugo <32584307+cristianohugo@users.noreply.github.com>
2021-07-16 01:16:22 -0300

    Instalado template AdmiLTE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from "react";
-import logo from './logo.png';
+//import logo from './logo.png';
 import './App.css';
+import Header from "./Header";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import Dashboard from "./Dashboard";
 
 function App() {
-
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch(process.env.REACT_APP_API_URL+"/home")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Carregando..." : data}</p>
-      </header>
+    <div class="wrapper">
+      <Header/>
+      <Menu/>
+      <Dashboard/>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+  /*>
+  function App() {
+  
+    const [data, setData] = React.useState(null);
+  
+    React.useEffect(() => {
+      fetch(process.env.REACT_APP_API_URL+"/home")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+    }, []);
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{!data ? "Carregando..." : data}</p>
+        </header>
+      </div>
+    );
+  }
+  */
+
+  export default App;
