4edc8764fbecf87e78d8b8098915304d890b5c4f
cristianohugo <32584307+cristianohugo@users.noreply.github.com>
2021-07-17 22:41:42 -0300

    Update CadastroReceitas.js

--- src/Pages/CadastroReceitas.js
+++ src/Pages/CadastroReceitas.js
@@ -4,85 +4,53 @@ export default class CadastroReceitas extends Component {
     render() {
         return (
             <div>
-                <section className="content">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="card card-primary">
-                                <div className="card-header">
-                                    <h3 className="card-title">General</h3>
-                                    <div className="card-tools">
-                                        <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                            <i className="fas fa-minus" />
-                                        </button>
-                                    </div>
+
+                <div className="content-wrapper" style={{ minHeight: '1345.6px' }}>
+                    <section className="content-header">
+                        <div className="container-fluid">
+                            <div className="row mb-2">
+                                <div className="col-sm-6">
+                                    <h1>General Form</h1>
                                 </div>
-                                <div className="card-body">
-                                    <div className="form-group">
-                                        <label htmlFor="inputName">Project Name</label>
-                                        <input type="text" id="inputName" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputDescription">Project Description</label>
-                                        <textarea id="inputDescription" className="form-control" rows={4} defaultValue={""} />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputStatus">Status</label>
-                                        <select id="inputStatus" className="form-control custom-select">
-                                            <option selected disabled>Select one</option>
-                                            <option>On Hold</option>
-                                            <option>Canceled</option>
-                                            <option>Success</option>
-                                        </select>
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputClientCompany">Client Company</label>
-                                        <input type="text" id="inputClientCompany" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputProjectLeader">Project Leader</label>
-                                        <input type="text" id="inputProjectLeader" className="form-control" />
-                                    </div>
+                                <div className="col-sm-6">
+                                    <ol className="breadcrumb float-sm-right">
+                                        <li className="breadcrumb-item"><a href="#">Dashbord</a></li>
+                                        <li className="breadcrumb-item active">Título da Página</li>
+                                    </ol>
                                 </div>
-                                {/* /.card-body */}
                             </div>
-                            {/* /.card */}
                         </div>
-                        <div className="col-md-6">
-                            <div className="card card-secondary">
-                                <div className="card-header">
-                                    <h3 className="card-title">Budget</h3>
-                                    <div className="card-tools">
-                                        <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                            <i className="fas fa-minus" />
-                                        </button>
+                    </section>
+                    <section className="content">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <div className="card card-primary">
+                                        <div className="card-header">
+                                            <h3 className="card-title">Título da Página</h3>
+                                        </div>
+                                        <form action="URL_BACK_END" name="nome-form" id="id-form">
+                                            <div className="card-body">
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputEmail1">Nome Label 1</label>
+                                                    <input type="text" name="nome_coluna_banco1" className="form-control" id="nome_coluna_banco" placeholder="Digite o Valor" />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="exampleInputEmail1">Nome Label 2</label>
+                                                    <input type="text" name="nome_coluna_banco2" className="form-control" id="nome_coluna_banco" placeholder="Digite o Valor" />
+                                                </div>
+                                            </div>
+                                            <div className="card-footer">
+                                                <button type="submit" className="btn btn-primary">Salvar</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
-                                <div className="card-body">
-                                    <div className="form-group">
-                                        <label htmlFor="inputEstimatedBudget">Estimated budget</label>
-                                        <input type="number" id="inputEstimatedBudget" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputSpentBudget">Total amount spent</label>
-                                        <input type="number" id="inputSpentBudget" className="form-control" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="inputEstimatedDuration">Estimated project duration</label>
-                                        <input type="number" id="inputEstimatedDuration" className="form-control" />
-                                    </div>
-                                </div>
-                                {/* /.card-body */}
                             </div>
-                            {/* /.card */}
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <a href="#" className="btn btn-secondary">Cancel</a>
-                            <input type="submit" defaultValue="Create new Porject" className="btn btn-success float-right" />
-                        </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
+
 
             </div>
         )
