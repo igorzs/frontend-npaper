7d33ecd91a4cffcac90d53183390a0479a8f86f5
cristianohugo <32584307+cristianohugo@users.noreply.github.com>
2021-07-19 23:16:49 -0300

    update cadatro de receitas e listagem

--- src/Pages/CadastroReceitas.js
+++ src/Pages/CadastroReceitas.js
@@ -30,24 +30,26 @@ export default class CadastroReceitas extends Component {
                                                         <label htmlFor="exampleInputEmail1">Valor</label>
                                                         <input type="text" name="nome_coluna_banco1" className="form-control" id="nome_coluna_banco" placeholder="Digite o Valor" />
                                                     </div>
-                                                    <div class="col-3">
+                                                    <div class="col-4">
                                                         <label htmlFor="exampleInputEmail1">Descrição</label>
                                                         <input type="text" name="nome_coluna_banco2" className="form-control" id="nome_coluna_banco" placeholder="Digite a Descrição" />
                                                     </div>
                                                 </div>
-                                                <div className="form-group">
-                                                    <label htmlFor="exampleInputEmail1">Data</label>
-                                                    <input type="text" name="nome_coluna_banco2" className="form-control" id="nome_coluna_banco" placeholder="Digite a Data" />
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Categoria</label>
-                                                    <select className="form-control">
-                                                        <option>Remuneração</option>
-                                                        <option>Bônus</option>
-                                                        <option>Rendimento</option>
-                                                        <option>Empréstimo</option>
-                                                        <option>Outras Rendas</option>
-                                                    </select>
+                                                <div className="row">
+                                                    <div className="col-3">
+                                                        <label htmlFor="exampleInputEmail1">Data</label>
+                                                        <input type="text" name="nome_coluna_banco2" className="form-control" id="nome_coluna_banco" placeholder="Digite a Data" />
+                                                    </div>
+                                                    <div className="col-4">
+                                                        <label>Categoria</label>
+                                                        <select className="form-control">
+                                                            <option>Remuneração</option>
+                                                            <option>Bônus</option>
+                                                            <option>Rendimento</option>
+                                                            <option>Empréstimo</option>
+                                                            <option>Outras Rendas</option>
+                                                        </select>
+                                                    </div>
                                                 </div>
                                                 <div className="form-group">
                                                     <div className="form-check">
